ed02eb949b51a609a34ee2d3bc2467d413ab54a3
Duong Vu <hdvu98@users.noreply.github.com>
2020-01-01 15:08:11 +0700

    AbstractSqlConnection, SqlServerConnection

--- DAMFramework/Connection/AbstractSqlConnection.cs
+++ DAMFramework/Connection/AbstractSqlConnection.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAM_ORMFramework.Query;
 
 namespace DAM_ORMFramework.Mapping
 {
-    class AbstractSqlConnection
+    abstract class AbstractSqlConnection
     {
+        protected string cnnString { get; set; }
+
+        public abstract void Open();
+        public abstract void Close();
+        public abstract WhereClause<T> Select<T>() where T : new();
+        public abstract int Insert<T>(T obj) where T : new();
+        public abstract int Update<T>(T obj) where T : new();
+        public abstract int Delete<T>(T obj) where T : new();
+        public abstract List<T> ExecuteQuery<T>(string query) where T : new();
+        public abstract void ExecuteNonQuery(string query);
+        public abstract List<T> ExecuteQueryNotRelationship<T>(string query) where T : new();
+
     }
 }
